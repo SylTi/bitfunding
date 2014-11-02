4f84a393d6e085ea480862358e33bcd98333b394
Hugo <hugo@placeloop.com>
2014-11-03 00:15:50 +0100

    [feature #3] dashboard page: now showing current btc balance

--- client/app/account/dashboard/dashboard.controller.js
+++ client/app/account/dashboard/dashboard.controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('bitCrowdFundsApp')
-  .controller('DashboardCtrl', function ($scope, MyProjects, DashboardRes) {
+  .controller('DashboardCtrl', function ($scope, MyProjects, DashboardRes, Auth) {
     $scope.datas = DashboardRes.query();
     $scope.projects = MyProjects.query();
+    $scope.userData = Auth.getCurrentUser();
   });
