4c5abebb2e1faa39a7fbd6d110f05682ebebf512
Hugo <hugo@placeloop.com>
2014-11-07 14:42:49 +0100

    [feature] redirect to /projects page when project not found

--- client/app/projects/project/project.controller.js
+++ client/app/projects/project/project.controller.js
@@ -1,29 +1,33 @@
 'use strict';
 
 angular.module('bitCrowdFundsApp')
-  .controller('ProjectCtrl', function ($scope, $http, $routeParams, ProjectRes, Auth) {
+  .controller('ProjectCtrl', function ($scope, $http, $routeParams, ProjectRes, Auth, $location) {
     var currentUser = Auth.getCurrentUser();
-    $scope.currentProject = ProjectRes.get({name: $routeParams.projectName});
-    console.log($scope.currentProject);
-    console.log($routeParams.projectName);
-    $scope.projectName = $routeParams.projectName;
-
+    $scope.currentProject =  false;
     $scope.resContrib = '';
+    $scope.projectName = '';
+
+    $scope.init = function() {
+        ProjectRes.get({name: $routeParams.projectName}).
+            $promise.then(function(datas) {
+                $scope.currentProject = datas;
+                $scope.projectName = $routeParams.projectName;
+            }, function(err){
+                $location.path('/projects');
+        });
+    };
 
     $scope.contribute = function()
     {
-    	console.log("test");
       var contribSatoshi = (parseInt($scope.contribAmount)*100000000);
     	$http.post('api/projects/'+$scope.projectName+'/contrib', {userId: currentUser._id, userName: currentUser.name, amount: contribSatoshi})
     	.success(function(data, status, headers, config)
     	{
     		$scope.resContrib = 'You just contributed '+$scope.contribAmount+' BTC to ' + $scope.projectName;
-    		//$scope.$apply();
     		$scope.currentProject = data;
-    		console.log("contrib ok ");
     	}).error(function(data, status, headers, config)
     	{
-    		$scope.resContrib = 'Something wrong happend';
+    		$scope.resContrib = 'Something wrong happend : ' + data;
     	});
     };
   });
