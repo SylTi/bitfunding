baef1049a07079dd69dbb31f84d6ee8636e3ef04
Hugo <hugo@placeloop.com>
2014-11-07 15:58:38 +0100

    [ui] edit link on project page (when logged user is the project owner)

--- client/app/projects/project/project.controller.js
+++ client/app/projects/project/project.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('bitCrowdFundsApp')
   .controller('ProjectCtrl', function ($scope, $http, $routeParams, ProjectRes, Auth, $location) {
-    var currentUser = Auth.getCurrentUser();
+    $scope.currentUser = Auth.getCurrentUser();
     $scope.currentProject =  false;
     $scope.resContrib = '';
     $scope.projectName = '';
@@ -21,7 +21,7 @@ angular.module('bitCrowdFundsApp')
     {
         var repl = $scope.contribAmount.replace(',', '.');
         var contribSatoshi = parseFloat(repl*100000000);
-    	$http.post('api/projects/'+$scope.currentProject.slug+'/contrib', {userId: currentUser._id, userName: currentUser.name, amount: contribSatoshi})
+    	$http.post('api/projects/'+$scope.currentProject.slug+'/contrib', {userId: $scope.currentUser._id, userName: $scope.currentUser.name, amount: contribSatoshi})
     	.success(function(data, status, headers, config)
     	{
     		$scope.resContrib = 'You just contributed '+$scope.contribAmount+' BTC to ' + $scope.projectName;
