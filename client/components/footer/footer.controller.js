b114307b8447ef6120fb33bf445a09ce927b9054
SylTi <sylvain.tissier@gmail.com>
2014-11-14 18:42:55 +0100

    [bug] footer bug (close #33)

--- client/components/footer/footer.controller.js
+++ client/components/footer/footer.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('bitCrowdFundsApp')
   .controller('FooterCtrl', function ($scope, $http, CoindeskAPI) {
-    $scope.coindesk_error = false
+    $scope.coindesk_error = false;
     $scope.current_prices = {};
     $scope.current_prices = CoindeskAPI.query();
   });
