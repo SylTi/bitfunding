a377af77f288a675817b07a22ba8bb839f78eece
SylTi <sylvain.tissier@gmail.com>
2014-11-21 15:04:35 +0100

    adding balance to $ in dashboard

--- client/app/account/dashboard/dashboard.controller.js
+++ client/app/account/dashboard/dashboard.controller.js
@@ -2,10 +2,16 @@
 
 
 angular.module('bitCrowdFundsApp')
-  .controller('DashboardCtrl', function ($scope, MyProjects, DashboardRes, Auth) {
+  .controller('DashboardCtrl', function ($scope, MyProjects, DashboardRes, Auth, CoindeskAPI) {
     $scope.datas = DashboardRes.query();
     $scope.projects = MyProjects.query();
     $scope.userData = Auth.getCurrentUser();
+    $scope.currentPrices = {};
+    $scope.currentPrices = CoindeskAPI.query(function (data)
+      {
+        $scope.currentPriceD = ($scope.userData.balance / 100000000) * data.bpi.USD.rate;
+
+      });
     $scope.unconfirmed = function()
     {
       if ($scope.userData.unconfirmedBalance > 0)
@@ -17,4 +23,10 @@ angular.module('bitCrowdFundsApp')
         return false;
       }
     };
+
+    $scope.changePrice = function()
+    {
+     // $scope.currentPrices.bpi.USD.rate;
+    };
+
   });
