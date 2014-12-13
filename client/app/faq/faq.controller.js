9ea28814c4ba181755c7f45344d4c935cff4d683
SylTi <sylvain.tissier@gmail.com>
2014-12-13 17:00:16 +0100

    [enhancement] adding some footer pages #53

--- client/app/faq/faq.controller.js
+++ client/app/faq/faq.controller.js
@@ -2,5 +2,27 @@
 
 angular.module('bitCrowdFundsApp')
   .controller('FaqCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    $scope.oneAtATime = true;
+    $scope.groups = [
+      {
+        title: 'What is Bitcoin',
+        content: 'Bitcoin is a software-based online payment system described by Satoshi Nakamoto in 2008 and introduced as open-source software in 2009. Payments are recorded in a public ledger using its own unit of account, which is also called bitcoin.'
+      },
+      {
+        title: 'Why Bitfunding vs XXX (KickStarter...)',
+        content: '<ul><li>We are cheaper (2% fee on completed projects)</li><li>We will use a multiway party transaction signature so the user remain in control of is funds at all time.</li></ul>'
+      }
+    ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
   });
